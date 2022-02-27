c916f6537c9481f915195f395980e120380c6e92
amrutakadu <amrutakadu99@gmail.com>
2022-02-27 23:27:46 +0530

    Added gitModel in models.py - commit by amruta

--- login_app/models.py
+++ login_app/models.py
@@ -6,3 +6,6 @@ class Credentials(models.Model):
     uname = models.CharField(max_length = 100, null = True)
     pswd = models.CharField(max_length = 100, null = True)
     email = models.CharField(max_length = 20, null = True)
+
+class gitModel(models.Model):
+    gitcmd = models.CharField(max_length = 50, null = True)
