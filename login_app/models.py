9dbbabfbda620c1f5ed222bf5930fed632c24eb6
amrutakadu <amrutakadu99@gmail.com>
2022-02-27 18:47:03 +0530

    Files added/staged - commit by Amruta

---
+++ login_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Credentials(models.Model):
+    uname = models.CharField(max_length = 100, null = True)
+    pswd = models.CharField(max_length = 100, null = True)
